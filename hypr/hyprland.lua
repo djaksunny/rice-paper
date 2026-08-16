e3bd9cd86021d79a379d3e6a3daf2f512fbca52e
Alex Sun <alexsun0725@gmail.com>
2026-08-16 00:47:56 -0500

    fix: restructure dirs for hypr

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,15 +1,10 @@
--- Keybinds
-require("defaults")
-require("keybinds")
+require("config.defaults")
+require("config.keybinds")
+require("config.monitors")
+require("config.decoration")
 
--- Monitors
-require("monitors")
-
--- Decoration
-require("decoration")
-
--- On-Start Launch
 hl.on("hyprland.start", function ()
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("waybar")
 end)
+
